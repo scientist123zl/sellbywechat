2a645a8a97a0e51995afa5bdfb54ec7df04e5967
scientist123zl <zhulei678678@163.com>
2020-02-26 17:39:01 +0800

    买家订单service

--- src/main/java/com/imooc/sell/dto/OrderDTO.java
+++ src/main/java/com/imooc/sell/dto/OrderDTO.java
@@ -1,24 +1,17 @@
-package com.imooc.sell.dataobject;
+package com.imooc.sell.dto;
 
+import com.imooc.sell.dataobject.OrderDetail;
 import com.imooc.sell.enums.OrderStatusEnum;
 import com.imooc.sell.enums.PayStatusEnum;
 import lombok.Data;
-import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Transient;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-@Entity
 @Data
-@DynamicUpdate
-public class OrderMaster {
-
+public class OrderDTO {
     //订单Id
-    @Id
     private String orderId;
 
     //买家名字
@@ -37,15 +30,17 @@ public class OrderMaster {
     private BigDecimal orderAmount;
 
     //订单状态  默认为0新下单
-    private Integer orderStatus= OrderStatusEnum.NEW.getCode();
+    private Integer orderStatus;
 
     //支付状态，默认为0未支付
-    private Integer payStatus= PayStatusEnum.WAIT.getCode();
+    private Integer payStatus;
 
     //订单创建时间
     private Date createTime;
 
     //订单更新时间
     private Date updateTime;
+
+    List<OrderDetail> orderDetailList;
 
 }
--- src/main/java/com/imooc/sell/enums/OrderStatusEnum.java
+++ src/main/java/com/imooc/sell/enums/OrderStatusEnum.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum OrderStatusEnum {
     NEW(0,"新订单"),
-    FINISH(1,"完结"),
+    FINISHED(1,"完结"),
     CANCEL(2,"已取消"),
     ;
 
